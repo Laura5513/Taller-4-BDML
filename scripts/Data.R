437369856efe042852b958ab60f149a48b63eb94
Laura5513 <lauramanuela.rodriguez@gmail.com>
2023-03-13 21:59:10 -0500

    Creación script limpieza datos

--- scripts/Data.R
+++ scripts/Data.R
@@ -6,3 +6,36 @@
 #                                  Brayan Alexander Vargas Rojas                       #
 #                          Fuente: Tweets                                              #
 #**************************************************************************************#
+
+# Limpiar el espacio
+rm(list = ls(all.names = TRUE))
+
+# ------------------------------------------------------------------------------------ #
+# Cargar librerias.
+# ------------------------------------------------------------------------------------ #
+
+setwd("C:/Users/lmrod/OneDrive/Documentos/GitHub/Taller-4-BDML")
+#setwd("C:/Users/nicol/Documents/GitHub/Repositorios/Taller-4-BDML")
+#setwd("/Users/bray/Desktop/Big Data/Talleres/Taller-4-BDML")
+
+list.of.packages = c("pacman", "readr","tidyverse", "dplyr", "arsenal", "fastDummies", 
+                     "caret", "glmnet", "MLmetrics", "skimr", "plyr", "stargazer", 
+                     "ggplot2", "plotly", "corrplot", "Hmisc", "sf", "tmaptools", 
+                     "osmdata", "leaflet", "rgeos")
+
+
+new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+sapply(list.of.packages, require, character.only = TRUE)
+
+
+# ------------------------------------------------------------------------------------ #
+# 1. Descripción del problema
+# ------------------------------------------------------------------------------------ #
+
+# Predecir qué político posteó cada tweet en el test set (Claudia Lopez, Gustavo
+#Petro o Alvaro Uribe)
+
+# ------------------------------------------------------------------------------------ #
+# 2. Data
+# ------------------------------------------------------------------------------------ #
