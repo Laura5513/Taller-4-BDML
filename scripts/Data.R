e00edb3df398e57c84ddd16a49e0492c0a49185d
Laura5513 <lauramanuela.rodriguez@gmail.com>
2023-03-13 21:54:45 -0500

    Create Data.R

---
+++ scripts/Data.R
@@ -0,0 +1,8 @@
+#**************************************************************************************#
+#                                    TALLER 3 BDML                                     #
+#                        Uniandes: Sofia Charry Tobar                                  #
+#                                  Laura Manuela Rodríguez Morales                     #
+#                                  Nicol Valeria Rodríguez Rodríguez                   #
+#                                  Brayan Alexander Vargas Rojas                       #
+#                          Fuente: Tweets                                              #
+#**************************************************************************************#
