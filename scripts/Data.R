b5470fbdb4720e57147ec329aa6173c5f1aa5f41
Laura5513 <lauramanuela.rodriguez@gmail.com>
2023-03-20 16:40:22 -0500

    Procesamiento conjunto script

--- scripts/Data.R
+++ scripts/Data.R
@@ -14,9 +14,9 @@ rm(list = ls(all.names = TRUE))
 # Cargar librerias.
 # ------------------------------------------------------------------------------------ #
 
-#setwd("C:/Users/lmrod/OneDrive/Documentos/GitHub/Taller-4-BDML")
+setwd("C:/Users/lmrod/OneDrive/Documentos/GitHub/Taller-4-BDML")
 #setwd("C:/Users/nicol/Documents/GitHub/Repositorios/Taller-4-BDML")
-setwd("/Users/bray/Desktop/Big Data/Talleres/Taller-4-BDML")
+#setwd("/Users/bray/Desktop/Big Data/Talleres/Taller-4-BDML")
 
 
 list.of.packages = c("pacman", "readr","tidyverse", "dplyr", "tidyr", "fastDummies",
@@ -34,7 +34,7 @@ sapply(list.of.packages, require, character.only = TRUE)
 # ------------------------------------------------------------------------------------ #
 
 # Predecir qué político posteó cada tweet en el test set (Claudia Lopez, Gustavo
-#Petro o Alvaro Uribe)
+# Petro o Alvaro Uribe)
 
 # ------------------------------------------------------------------------------------ #
 # 2. Data
@@ -57,6 +57,102 @@ test <- read_csv("./data/test.csv", col_types = cols(
 ))
 
 # 2.1 Preprocesamiento --------------------------------------------------------
+
+#Para tener las mismas columnas en train y test, preprocesaremos una base con ambas juntas.
+#Generamos un identificador cuando ya las vayamos a utilizar por separado en los modelos
+
+train$train <- 1 
+test$train <- 0
+
+# Para tener mismo número de columnas
+train1 <- train[,-2]
+head(train1)
+dim(train1) # 9349 observaciones
+head(test)
+dim(test) # 1500 observaciones
+
+# Juntamos train y test
+junto <- rbind(train1, test)
+dim(junto)
+
+#Después de la posición 9349 en junto, tenemos las observaciones de test
+junto$train[9349]
+junto$train[9350]
+
+
+p_load(SnowballC)
+p_load(Matrix)
+p_load(NLP)
+
+# Eliminamos tildes y caracteres especiales del español
+junto <- stri_trans_general(str = junto$text, id = "Latin-ASCII")
+# Sin URL
+junto <- gsub("http\\S+", "", junto)
+# Sin tejuntoto de retwitteo
+junto <- gsub("(rt|via)((?:\\b\\W*@\\w+)+)", "", junto)
+# Quitamos @ (@usuario)
+junto <- gsub("@\\w+", "", junto)
+# Quitamos espacios en blanco extras
+junto <- gsub("\\s+", " ", junto)
+# Recortar los espacios en blanco iniciales y finales
+junto <- gsub("^\\s+|\\s+$", "", junto)
+# Remover emojis
+junto <- gsub("[^\x01-\x7F]", "", junto)
+# Remover puntuación 
+junto <- gsub("[[:punct:]]", "", junto)
+# Remover números
+junto <- gsub('[0-9]+', '', junto)
+# Quitamos stop words
+p_load(stopwords)
+# Descargamos la lista de las stopwords en español de dos fuentes diferentes y las combinamos
+lista_palabras1 <- stopwords(language = "es", source = "snowball")
+lista_palabras2 <- stopwords(language = "es", source = "nltk")
+lista_palabras <- union(lista_palabras1, lista_palabras2)
+
+junto <- removeWords(junto, lista_palabras)
+
+length(junto) #10849
+
+# Stemmizamos 
+junto <- wordStem(junto, language = "spanish")
+length(junto)
+corpus_junto <- Corpus(VectorSource(junto))
+
+dtm_idf_junto<-DocumentTermMatrix(junto,control=list(weighting=weightTfIdf))
+inspect(dtm_idf_junto[100:103,]) # Los documentos 411 1240 2152 2682 2806 3558 4715 6493 9036 9460 quedan vacíos
+
+# Eliminamos términos que son poco frecuentes en todo el corpus
+dtm_idf_junto <- removeSparseTerms(dtm_idf_junto, sparse = 0.999)
+inspect(dtm_idf_junto[100:103,]) 
+
+dim(dtm_idf_junto)
+
+# Test final, recoge las observaciones correspondientes a test
+test_final1 <- as.data.frame(as.matrix(dtm_idf_junto[9350:nrow(dtm_idf_junto),]), stringsAsFactors = FALSE)
+test_final1 <- cbind(test$id, test_final1)
+colnames(test_final1) <- c("id", colnames(test_final1[2:ncol(test_final1)]))
+head(test_final1)
+
+# Train final, recoge las observaciones correspondientes a train
+dummy <- ifelse(train$name=="Petro", 1, ifelse(train$name=="Lopez", 2, ifelse(train$name=="Uribe", 3, 0)))
+train_final1 <- as.data.frame(as.matrix(dtm_idf_junto[1:9349, ]), stringsAsFactors=False)
+train_final1 <- train_final1[, intersect(colnames(test_final1), colnames(train_final1))]
+train_final1 <- cbind(train$id, train$name,  dummy, train_final1)
+colnames(train_final1) <- c("id", "name", "dummy", colnames(train_final1[4:ncol(train_final1)]))
+
+head(test_final1)
+dim(test_final1)
+head(train_final1)
+dim(train_final1)
+
+# La única diferencia es que train tiene dumy y name
+diff_cols <- setdiff(names(train_final1), names(test_final1))
+
+# Train
+write.csv(train_final1,"./data/train_final1.csv", row.names = FALSE)
+
+# Test
+write.csv(test_final1,"./data/test_final1.csv", row.names = FALSE)
 
 # 2.1.1 ----------------------------  Train -----------------------------------
 
